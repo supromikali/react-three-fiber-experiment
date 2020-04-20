438193ea3e2aa5ab5cbf2b751ec32d4f0324258f
Alexander Solovyev <supromikali@gmail.com>
2020-04-20 08:31:17 +0300

    add simple bouncing animation

--- src/index.js
+++ src/index.js
@@ -17,10 +17,23 @@ const generatePositions = () => {
 
 const positions = generatePositions();
 
-function Box(props) {
+let bounceTarget = 2;
+
+function Item(props) {
     const mesh = useRef();
 
-    useFrame(() => (mesh.current.rotation.y = mesh.current.rotation.y += 0.01))
+    useFrame(() => {
+        mesh.current.rotation.y = mesh.current.rotation.y += 0.01
+
+        // simple bouncing animation
+        if (mesh.current.scale.x > 2) {
+            bounceTarget = 0.2
+        }
+        if (mesh.current.scale.x < 0.2) {
+            bounceTarget = 2
+        }
+        mesh.current.scale.x += bounceTarget === 2 ? 0.01 : -0.01;
+    });
 
     return (
         <mesh
@@ -38,7 +51,7 @@ ReactDOM.render(
         <Canvas>
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
-            {positions.map(coords => <Box key={coords} position={coords} />)}
+            {positions.map(coords => <Item key={coords} position={coords} />)}
         </Canvas>
     </div>,
     document.getElementById('root')
